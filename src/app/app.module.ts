894e35923047a6d7f5c13d8bf581b03f7ff38812
aleksandar <todor.petrovgrad@yahoo.com>
2018-01-16 19:02:26 +0100

    1 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { CarsComponentComponent } from './components/cars-component/cars-component.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+const appRoutes: Routes = [
+  { path: 'cars', component: CarsComponentComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    CarsComponentComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
